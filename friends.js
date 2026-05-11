ee58597a3d1f4ca7c9ccef59ecc6fa16d2a08026
Monasri Kore <koremonasri@gmail.com>
2026-05-11 09:52:49 +0530

    Update friends.js

--- friends.js
+++ friends.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
-const { run, get, all } = require('../db');
+const { run, get, all } = require('./db');
 const authMiddleware = require('../middleware/auth');
 
 router.use(authMiddleware);
